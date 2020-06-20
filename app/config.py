e0d1254232870ae115f26b7316fe3fb37ba30a0a
dbartleman <drewbartleman@gmail.com>
2020-06-19 17:02:35 -0700

    made models, next is routes

--- app/config.py
+++ app/config.py
@@ -5,4 +5,4 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "key-for-devs"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    # SQLALCHEMY_DATABASE_URI = "postgresql://soundspace_user:password@localhost/soundspace_db"
+    
--- app/models.py
+++ app/models.py
@@ -3,3 +3,60 @@ from werkzeug.security import check_password_hash
 from sqlalchemy import Table
 
 db = SQLAlchemy()
+
+
+class Favorite(db.Model):
+    __tablename__ = "favorite"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    song_id = db.Column(db.Integer, db.ForeignKey("albums.id"), nullable=False)
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(50), nullable=False)
+
+    favorites = db.relationship(
+        "Album", secondary="favorite")
+
+
+class Artist(db.Model):
+    __tablename__ = "artists"
+
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(500), nullable=True, unique=False)
+
+    albums = db.relationship("Album", back_populates="artist")
+
+
+class Album(db.Model):
+    __tablename__ = "albums"
+
+    id = db.Column(db.Integer, primary_key=True)
+    artist_id = db.Column(db.Integer, db.ForeignKey("artists.id"), nullable=False)
+    title = db.Column(db.String(30), nullable=False)
+    description = db.Column(db.String(1000))
+
+    songs = db.relationship("Song", back_populates="album")
+    artist = db.relationship("Artist", back_populates="albums")
+    favorites = db.relationship("Favorite")
+
+
+class Song(db.Model):
+    __tablename__ = "songs"
+    id = db.Column(db.Integer, primary_key=True)
+    album_id = db.Column(db.Integer, db.ForeignKey("albums.id"), nullable=False)
+    url = db.Column(db.String(200), nullable=False)
+    
+    album = db.relationship("Album", back_populates="songs")
+
+
+
+
