87d82f96e0958c60bdb1f5a091103950950bded2
dbartleman <drewbartleman@gmail.com>
2020-06-24 00:09:36 -0700

    song route

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, redirect
 from .config import Config
 from .models import db
-# from flask_migrate import Migrate
-# from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# db.init_app(app)
-# Migrate(app, db)
-# CORS(app)
+db.init_app(app)
+Migrate(app, db)
+CORS(app)
 
 from .routes import user, song, album, artist, favorites  # noqa
 app.register_blueprint(user.bp)
@@ -16,6 +16,6 @@ app.register_blueprint(song.bp)
 app.register_blueprint(album.bp)
 app.register_blueprint(artist.bp)
 app.register_blueprint(favorites.bp)
-app.register_blueprint(song.searchbp)
+# app.register_blueprint(song.searchbp)
 app.register_blueprint(album.searchbp)
-app.register_blueprint(artist.searchbp)
+# app.register_blueprint(artist.searchbp)
---
+++ app/routes/song.py
@@ -0,0 +1,42 @@
+from flask import Blueprint, request
+from ..models import Song, db, Album
+
+
+bp = Blueprint("songs", __name__, url_prefix="")
+
+
+@bp.route("/songs")
+def get_songs():
+    songs = Song.query.all()
+    data = [{
+        "id": song.id,
+        "album_id": song.album_id,
+        "url": song.url
+    } for song in songs]
+    return {"data": data}
+
+
+@bp.route("/songs/<albumid>")
+def get_songs_by_album(albumid):
+    songs = Song.query.filter_by(album_id=albumid).all()
+    data = {}
+    for song in songs:
+        data[song.id] = {
+            "id": song.id,
+            "album_id": song.album_id,
+            "url": song.url
+        }
+    return {"data": data}
+
+
+@bp.route("/songs/<int:albumid>/<int:songid>")
+# may go unused
+def get_song_in_album(albumid, songid):
+    song = Song.query.filter_by(album_id=albumid).filter_by(id=songid).first()
+    data = {
+        "id": song.id,
+        "album_id": song.album_id,
+        "url": song.url
+    }
+    return {"data": data}
+
