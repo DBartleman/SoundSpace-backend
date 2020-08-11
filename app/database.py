72038a2c373acb79b0d58f7e55dedb289b34f5e8
dbartleman <drewbartleman@gmail.com>
2020-08-11 00:30:00 -0700

    more database work, more seed data

--- app/database.py
+++ app/database.py
@@ -6,40 +6,54 @@ load_dotenv()
 with app.app_context():
     db.drop_all()
     db.create_all()
-
+    
+    # Demo user
     user = User(email="demo@email.com", username="demo", password="demo")
 
+    # artists
     artist1 = Artist(
         name="Air", description="Air are a French music duo from Versailles, consisting of Nicolas Godin and Jean-Benoît Dunckel.")
-    artist2 = Artist(name="", description="")
-    artist3 = Artist(name="", description="")
-    artist4 = Artist(name="", description="")
-    artist5 = Artist(name="", description="")
-    artist6 = Artist(name="", description="")
-    artist7 = Artist(name="", description="")
-    artist8 = Artist(name="", description="")
-    artist9 = Artist(name="", description="")
-    artist10 = Artist(name="", description="")
-    artist11 = Artist(name="", description="")
-    artist12 = Artist(name="", description="")
-    artist13 = Artist(name="", description="")
+    artist2 = Artist(name="The Seatbelts", description="")
+    artist3 = Artist(name="Fred Hersch", description="")
+    artist4 = Artist(name="Gregory Privat", description="")
+    artist5 = Artist(name="Jim Lang", description="")
+    artist6 = Artist(name="Kat Epple and Bob Stohl", description="")
+    artist7 = Artist(name="Maxis", description="")
+    artist8 = Artist(name="Michael Naura", description="")
+    artist9 = Artist(name="Modern Jazz Quartet", description="")
+    artist10 = Artist(name="Ryo Fukui", description="")
 
-    album1 = Album(title="Moon Safari", description="Air's 1998 debut album Moon Safari is an influential masterpiece that not only started the international career of Jean-Benoît Dunckel and Nicolas Godin, but also found a loyal fanbase all over the world.")
+    # albums
+    moonSafari = Album(artist_id=1, title="Moon Safari", description="Air's 1998 debut album Moon Safari is an influential masterpiece that not only started the international career of Jean-Benoît Dunckel and Nicolas Godin, but also found a loyal fanbase all over the world.")
+    cowboyBebop = Album(artist_id=2, title="Cowboy Bebop", description="Cowboy Bebop is the first album created for the series, and the most easily categorized in terms of genre, as an outlet for many of the trademark bebop tracks. It begins with the show's theme song, \"Tank!\". The track \"Bad Dog No Biscuits\" opens with a cover of the Tom Waits composition \"Midtown\" before diverting in its interpretation.")
 
-    deck = Deck(title="Python", user_id=1, description="")
-    deck2 = Deck(title="Algorithms", user_id=1, description="")
-    cards = [
-        Card(deck_id=1, question="1 + 1", answer="2"),
-        Card(deck_id=1, question="2 + 2", answer=" 4"),
-        Card(deck_id=1, question="3 + 3", answer="6"),
-        Card(deck_id=1, question="4 + 4", answer="8"),
+    #songs
+    cowboyBebopSongs = [
+        Song(album_id=2, title="", url="", cover=""),
+        Song(album_id=2, title="", url="", cover=""),
+        Song(album_id=2, title="", url="", cover=""),
+        Song(album_id=2, title="", url="", cover=""),
+        Song(album_id=2, title="", url="", cover=""),
+        Song(album_id=2, title="", url="", cover=""),
+        Song(album_id=2, title="", url="", cover=""),
+        Song(album_id=2, title="", url="", cover=""),
+        Song(album_id=2, title="", url="", cover=""),
+        Song(album_id=2, title="", url="", cover=""),
+        Song(album_id=2, title="", url="", cover=""),
     ]
+    for song in cowboyBebopSongs:
+        db.session.add(song)
 
-    for card in cards:
-        db.session.add(card)
-
-    db.session.add(user)
-    db.session.add(deck)
-    db.session.add(deck2)
+    # Comitting
+    db.session.add(artist1)
+    db.session.add(artist2)
+    db.session.add(artist3)
+    db.session.add(artist4)
+    db.session.add(artist5)
+    db.session.add(artist6)
+    db.session.add(artist7)
+    db.session.add(artist8)
+    db.session.add(artist9)
+    db.session.add(artist10)
 
     db.session.commit()
